305caa375acf302cb58288e2870a89b16d7902b7
Angourisoft <lev.brovchenkov@gmail.com>
2019-12-26 20:20:06 +0300

    Forms example added

---
+++ Samples/GraphicExample/GraphicExample/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace GraphicExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.formsPlot1.Location = new System.Drawing.Point(0, 0);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(861, 489);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jump";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 546);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Samples/GraphicExample/GraphicExample/Form1.cs
@@ -0,0 +1,52 @@
+﻿using AngouriMath;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static FastExpression niceFunc;
+        static double t = 120;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var A = MathS.Var("A");
+            var B = MathS.Var("B");
+            var expr = B * MathS.Sin(B) * MathS.Pow(MathS.e, MathS.i * B * MathS.Cos(A));
+            niceFunc = expr.Compile(A, B);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var X = new List<double>();
+            var Y = new List<double>();
+            var A = t;
+            for(double B = 0; B < A; B += 0.1)
+            {
+                var res = niceFunc.Call(A, B);
+                X.Add(res.Re);
+                Y.Add(res.Im);
+            }
+            formsPlot1.plt.Clear();
+            formsPlot1.plt.PlotScatter(X.ToArray(), Y.ToArray());
+            formsPlot1.Render();
+            t += 0.0005;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t += 1.0;
+        }
+    }
+}
