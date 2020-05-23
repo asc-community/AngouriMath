d2b35e2bd78e301be3ef2c81b37b11e1eaa77cd3
Hadrian Tang <hadrianwttang@outlook.com>
2020-05-23 11:48:43 +0800

    Parentheses in case exponentiation is used

--- AngouriMath/Functions/Output/Latex.cs
+++ AngouriMath/Functions/Output/Latex.cs
@@ -215,7 +215,7 @@ namespace AngouriMath
                 var latex = ToString().Replace("Infinity", @"\infty ");
                 return
                     latex.Contains("E")
-                    ? latex.Replace("E+", "E").Replace("E-0", "E-").Replace("E", @"\times 10^{") + "}"
+                    ? @"\left(" + latex.Replace("E+", "E").Replace("E-0", "E-").Replace("E", @"\right)\times 10^{") + "}"
                     : latex;
             }
         }
