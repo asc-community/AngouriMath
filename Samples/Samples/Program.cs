789a46f791e855acaec9143a447c670e2d7d9007
Angourisoft <lev.brovchenkov@gmail.com>
2020-06-05 22:34:39 +0300

    A few refactorings

--- Samples/Samples/Program.cs
+++ Samples/Samples/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Samples
+﻿using System;
+using AngouriMath;
+using AngouriMath.Core.Numerix;
+
+namespace Samples
 {
     class Program
     {
         static void Main(string[] _)
         {
-
+            
         }
     }
 }
