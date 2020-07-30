9be0ed6b61b0b0292ad40d670e33b0217962a9e0
Angourisoft <lev.brovchenkov@gmail.com>
2020-07-30 15:32:18 +0300

    #170

--- Tests/UnitTests/Algebra/PatternTest.cs
+++ Tests/UnitTests/Algebra/PatternTest.cs
@@ -1,16 +1,67 @@
-﻿using AngouriMath;
+﻿using System;
+using AngouriMath;
 using AngouriMath.Core;
 using AngouriMath.Core.TreeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading;
+using AngouriMath.Extensions;
 
 namespace UnitTests.Algebra
 {
     [TestClass]
     public class PatternTest
     {
+        public bool BeingCompletedForLessThan(Action action, long timeoutMs)
+        {
+            var stopped = false;
+            var th = new Thread(() =>
+            {
+                action();
+                stopped = true;
+            });
+            th.Start();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while (!stopped && stopwatch.ElapsedMilliseconds < timeoutMs)
+                Thread.Sleep(5);
+            return stopped;
+        }
+
+        private const int GithubIssue170Timeout = 1600; // we don't know the target machines
+
+        // GitHub issue #170
         [TestMethod]
+        public void TestSimplifyHangs1()
+            =>
+                Assert.IsTrue(
+                    BeingCompletedForLessThan(
+                        () => "1 + 1 / x".Simplify(), GithubIssue170Timeout));
+
+        [TestMethod]
+        public void TestSimplifyHangs2()
+            =>
+                Assert.IsTrue(
+                    BeingCompletedForLessThan(
+                        () => "1 + 1 / (a + b)".Simplify(), GithubIssue170Timeout));
+
+        [TestMethod]
+        public void TestSimplifyHangs3()
+            =>
+                Assert.IsTrue(
+                    BeingCompletedForLessThan(
+                        () => "1 / x + 1".Simplify(), GithubIssue170Timeout));
+
+        [TestMethod]
+        public void TestSimplifyHangs4()
+            =>
+                Assert.IsTrue(
+                    BeingCompletedForLessThan(
+                        () => "(1 + 1 / x)^2 / (1 + 1 / x)".Simplify(), GithubIssue170Timeout));
+
+                [TestMethod]
         public void TestEnumerator1()
         {
             Entity expr = "a^x + b^y + c^z";
