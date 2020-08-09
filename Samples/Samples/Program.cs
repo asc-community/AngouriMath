dc80b10d592dc6be0307b2f886784cccc22cf37a
Angourisoft <lev.brovchenkov@gmail.com>
2020-08-09 14:25:06 +0300

    Parent to entity

--- Samples/Samples/Program.cs
+++ Samples/Samples/Program.cs
@@ -10,10 +10,11 @@ namespace Samples
     {
         static void Main(string[] _)
         {
-            Entity expr = "1+2x*-1+2x*2+x^2+2x+2x*-4+2x*4+2x*2x*-1+2x*2x*2+2x*x^2+x^2+x^2*-4+x^2*4+x^2*2*x*-1+x^2*2x*2+x^2*x^2";
-            Console.WriteLine(expr.Simplify());
-            //VariableEntity x = "x";
-            //var expr = MathS.Factorial(1 + x) / MathS.Factorial(-2 + x);
+            
+            Entity eqs = "log(x, 32) - 5";
+            var sols = eqs.SolveEquation("x");
+            Console.WriteLine(sols);
+            
         }
     }
 }
