be3f6a99bd85be2c93579d9639d0884e0465cb7d
Hadrian Tang <hadrianwttang@outlook.com>
2020-08-24 01:02:52 +0800

    Put slightly longer times

--- Tests/UnitTests/Common/PerformanceTest.cs
+++ Tests/UnitTests/Common/PerformanceTest.cs
@@ -20,9 +20,9 @@ namespace UnitTests.Common
         [Fact] public void Test1() => Measure(() => MathS.FromString("x + x^2 + x^3"), TimeSpan.FromMilliseconds(2));
         [Fact] public void Test2() => Measure(() => MathS.FromString("x + log(2, 4)^2 * sin(cos(sin(cos(5)))) + x^3"), TimeSpan.FromMilliseconds(2));
         [Fact] public void Test3() =>
-            MathS.Settings.NewtonSolver.As(new() { Precision = 400 }, () => Measure(() => (x.Pow(3) + x.Pow(2) - 4 * x - 4).SolveNt(x), TimeSpan.FromMilliseconds(80)));
+            MathS.Settings.NewtonSolver.As(new() { Precision = 400 }, () => Measure(() => (x.Pow(3) + x.Pow(2) - 4 * x - 4).SolveNt(x), TimeSpan.FromMilliseconds(120)));
         [Fact] public void Test4() => Measure(() => (x / x).Simplify(), TimeSpan.FromMilliseconds(0.05));
         [Fact] public void Test5() => Measure(() => (x * MathS.Pow(MathS.e, x) * MathS.Ln(x) - MathS.Sqrt(x / (x * x - 1))).Derive(x), TimeSpan.FromMilliseconds(0.5));
-        [Fact] public void Test6() => Measure(() => (x * MathS.Pow(MathS.e, x) * MathS.Ln(x) - MathS.Sqrt(x / (x * x - 1))).Derive(x).Substitute(x, 3).Eval(), TimeSpan.FromMilliseconds(15));
+        [Fact] public void Test6() => Measure(() => (x * MathS.Pow(MathS.e, x) * MathS.Ln(x) - MathS.Sqrt(x / (x * x - 1))).Derive(x).Substitute(x, 3).Eval(), TimeSpan.FromMilliseconds(20));
     }
 }
