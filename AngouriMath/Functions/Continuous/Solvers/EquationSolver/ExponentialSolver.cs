638ab4105a11457780ddd87c00651843052404e3
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2020-10-07 12:40:18 +0300

    added basic exponential solver (still needs improvements)

--- AngouriMath/Functions/Continuous/Solvers/EquationSolver/ExponentialSolver.cs
+++ AngouriMath/Functions/Continuous/Solvers/EquationSolver/ExponentialSolver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AngouriMath.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,36 @@ namespace AngouriMath.Functions.Algebra.AnalyticalSolving
     {
         internal static Entity.SetNode? SolveLinear(Entity expr, Entity.Variable x)
         {
-            var linearized = TreeAnalyzer.GatherLinearChildrenOverSumAndExpand(expr, e => e.Contains(x));
-            // TODO: exponential solver in future releases
-            return null;
+            var replacement = Entity.Variable.CreateTemp(expr.Vars);
+
+            Func<Entity, Entity> preparator = e => e switch
+            {
+                Entity.Powf(var @base, var arg) =>
+                    TreeAnalyzer.TryGetPolyLinear(arg, x, out var a, out var b) ?
+                        MathS.Pow(MathS.e, b) * MathS.Pow(MathS.Pow(MathS.e, x), MathS.Ln(@base) * a) : e,
+
+                _ => e
+            };
+
+            Func<Entity, Entity> replacer = e => e switch
+            {
+                Entity.Powf(var @base, var arg) =>
+                    @base == MathS.e && arg == x ?
+                        replacement : e,
+
+                _ => e,
+            };
+
+            expr = expr.Replace(preparator);
+            expr = expr.Replace(replacer);
+
+            if (expr.Contains(x)) return null; // cannot be solved, not a pure exponential
+
+            expr = expr.InnerSimplify();
+            if (AnalyticalEquationSolver.Solve(expr, replacement).IsFiniteSet(out var els))
+                return els.Select(sol => MathS.Pow(MathS.e, x).Invert(sol, x).ToSetNode()).Unite();
+            else
+                return null;
         }
     }
 }
