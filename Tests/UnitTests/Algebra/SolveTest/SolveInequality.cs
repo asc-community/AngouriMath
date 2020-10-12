51e1892ba78c1f7cb2ff91af81bac410c1b60bac
Angourisoft <lev.brovchenkov@gmail.com>
2020-10-12 23:09:30 +0300

    Test for inequality

---
+++ Tests/UnitTests/Algebra/SolveTest/SolveInequality.cs
@@ -0,0 +1,26 @@
+﻿using AngouriMath;
+using AngouriMath.Extensions;
+using static AngouriMath.Entity.Number;
+using Xunit;
+using System.Collections.Generic;
+using static AngouriMath.Entity;
+using static AngouriMath.Entity.Set;
+
+namespace UnitTests.Algebra
+{
+    public sealed class SolveInequality
+    {
+        [Theory]
+        [InlineData("x > 0", "(0; +oo)")]
+        [InlineData("x < 0", "(-oo; +oo)")]
+        [InlineData("x > 0 and x < 0", "{}")]
+        [InlineData("(x - 2)(x + 2) > 0", @"(-oo; -2) \/ (2; +oo)")]
+        [InlineData("(x - 2)(x + 2) < 0", "(-2; 2)")]
+        [InlineData("(x - 2)(x + 2) <= 0", "[-2; 2]")]
+        [InlineData("(x - a)(x + a) <= 0", "[-a; a]")]
+        public void Test(string initial, string expected)
+        {
+            Assert.Equal(expected.ToEntity(), initial.Solve("x").Simplify());
+        }
+    }
+}
