1d7b2c567c77739bd3fd30b7f8bbcdf556f81409
TheSeems <me@theseems.ru>
2020-11-06 19:57:42 +0300

    Add XorHashBuilder and hash for finite set

---
+++ AngouriMath/Core/XorHashBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngouriMath.Core
+{
+    struct XorHashBuilder
+    {
+        private int hash;
+
+        public XorHashBuilder Combine<T>(T obj)
+        {
+            if (obj == null)
+            {
+                return this;
+            }
+
+            hash ^= obj.GetHashCode();
+            return this;
+        }
+
+        public override int GetHashCode()
+        {
+            return hash;
+        }
+
+    }
+}
