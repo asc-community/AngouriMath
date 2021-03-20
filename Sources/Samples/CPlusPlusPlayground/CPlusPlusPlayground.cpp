97a10a488be974236e7523f56a63ecc3a2e740ea
Angourisoft <lev.brovchenkov@gmail.com>
2021-03-20 22:06:34 +0300

    Playground and wrapper added (as empty cmake projects)

---
+++ Sources/Samples/CPlusPlusPlayground/CPlusPlusPlayground.cpp
@@ -0,0 +1,12 @@
+﻿// CPlusPlusPlayground.cpp : Defines the entry point for the application.
+//
+
+#include "CPlusPlusPlayground.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Sources/Wrappers/AngouriMath.CPP/AngouriMath.CPP.cpp
@@ -0,0 +1,12 @@
+﻿// AngouriMath.CPP.cpp : Defines the entry point for the application.
+//
+
+#include "AngouriMath.CPP.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
