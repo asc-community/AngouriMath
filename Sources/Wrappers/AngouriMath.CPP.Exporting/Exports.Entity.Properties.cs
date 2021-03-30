97bf641973428cae2a7fbeafd4c6ea93e8b2b316
Angourisoft <lev.brovchenkov@gmail.com>
2021-03-30 23:58:26 +0300

    A few exported

--- Sources/Wrappers/AngouriMath.CPP.Exporting/Exports.Entity.Properties.cs
+++ Sources/Wrappers/AngouriMath.CPP.Exporting/Exports.Entity.Properties.cs
@@ -12,5 +12,23 @@ namespace AngouriMath.CPP.Exporting
             => ExceptionEncode(ref res, exprPtr,
                 exprPtr => NativeArray.Alloc(exprPtr.AsEntity.Nodes)
             );
+
+        [UnmanagedCallersOnly(EntryPoint = "entity_vars")]
+        public static NErrorCode EntityVars(ObjRef exprPtr, ref NativeArray res)
+            => ExceptionEncode(ref res, exprPtr,
+                exprPtr => NativeArray.Alloc(exprPtr.AsEntity.Vars)
+            );
+
+        [UnmanagedCallersOnly(EntryPoint = "entity_vars_and_constants")]
+        public static NErrorCode EntityVarsAndConstants(ObjRef exprPtr, ref NativeArray res)
+            => ExceptionEncode(ref res, exprPtr,
+                exprPtr => NativeArray.Alloc(exprPtr.AsEntity.VarsAndConsts)
+            );
+
+        [UnmanagedCallersOnly(EntryPoint = "entity_alternate")]
+        public static NErrorCode EntityVarsAdndConstants(ObjRef exprPtr, ref NativeArray res)
+            => ExceptionEncode(ref res, exprPtr,
+                exprPtr => NativeArray.Alloc(exprPtr.AsEntity.Alternate(4))
+            );
     }
 }
