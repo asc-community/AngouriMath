69e958f28c5c30cae1cfeabcc0dd604639293c2b
Angourisoft <lev.brovchenkov@gmail.com>
2021-03-31 12:34:47 +0300

    Operators =, <, >, <=, =>

--- Sources/Wrappers/AngouriMath.CPP.Exporting/Exports.Entity.Operators.cs
+++ Sources/Wrappers/AngouriMath.CPP.Exporting/Exports.Entity.Operators.cs
@@ -30,5 +30,35 @@ namespace AngouriMath.CPP.Exporting
             => ExceptionEncode(ref res, (left, right),
                 e => e.left.AsEntity / e.right.AsEntity
                 );
+
+        [UnmanagedCallersOnly(EntryPoint = "op_entity_less")]
+        public static NErrorCode Less(ObjRef left, ObjRef right, ref ObjRef res)
+            => ExceptionEncode(ref res, (left, right),
+                e => e.left.AsEntity < e.right.AsEntity
+                );
+
+        [UnmanagedCallersOnly(EntryPoint = "op_entity_greater")]
+        public static NErrorCode Greater(ObjRef left, ObjRef right, ref ObjRef res)
+            => ExceptionEncode(ref res, (left, right),
+                e => e.left.AsEntity > e.right.AsEntity
+                );
+
+        [UnmanagedCallersOnly(EntryPoint = "op_entity_less_or_equal")]
+        public static NErrorCode LessOrEqual(ObjRef left, ObjRef right, ref ObjRef res)
+            => ExceptionEncode(ref res, (left, right),
+                e => e.left.AsEntity <= e.right.AsEntity
+                );
+
+        [UnmanagedCallersOnly(EntryPoint = "op_entity_greater_or_equal")]
+        public static NErrorCode GreaterOrEqual(ObjRef left, ObjRef right, ref ObjRef res)
+            => ExceptionEncode(ref res, (left, right),
+                e => e.left.AsEntity >= e.right.AsEntity
+                );
+
+        [UnmanagedCallersOnly(EntryPoint = "op_entity_equal")]
+        public static NErrorCode OpEqual(ObjRef left, ObjRef right, ref NativeBool res)
+            => ExceptionEncode(ref res, (left, right),
+                e => e.left.AsEntity == e.right.AsEntity
+                );
     }
 }
---
+++ Sources/Wrappers/AngouriMath.CPP.Exporting/NativeStructs/NativeBool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AngouriMath.CPP.Exporting
+{
+    partial class Exports
+    {
+        public struct NativeBool
+        {
+            private int value;
+            public NativeBool(bool value)
+                => this.value = value ? 1 : 0;
+            public static NativeBool True => new(true);
+            public static NativeBool False => new(false);
+            public static implicit operator NativeBool(bool value)
+                => new(value);
+        }
+    }
+}
