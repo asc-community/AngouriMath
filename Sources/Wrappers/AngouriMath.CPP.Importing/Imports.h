4e9a18b698ecbb0b318500ec2732caee6f46dd34
WhiteBlackGoose <wbg@angouri.org>
2021-04-01 22:20:40 +0300

    Clang doesn't recognize declspec

--- Sources/Wrappers/AngouriMath.CPP.Importing/Imports.h
+++ Sources/Wrappers/AngouriMath.CPP.Importing/Imports.h
@@ -6,33 +6,38 @@ using namespace AngouriMath::Internal;
 
 extern "C"
 {
-    __declspec(dllimport) NativeErrorCode free_entity(EntityRef);
-    __declspec(dllimport) NativeErrorCode free_native_array(NativeArray);
-    __declspec(dllimport) NativeErrorCode free_error_code(NativeErrorCode);
-    __declspec(dllimport) NativeErrorCode free_string(String);
+# if defined(_MSC_VER) && !defined(__clang__) // clang on MSVC is a thing
+#  define DLL_CODE __declspec(dllimport)
+# else
+#  define DLL_CODE // nothing, you don't need it
+# endif
+    DLL_CODE NativeErrorCode free_entity(EntityRef);
+    DLL_CODE NativeErrorCode free_native_array(NativeArray);
+    DLL_CODE NativeErrorCode free_error_code(NativeErrorCode);
+    DLL_CODE NativeErrorCode free_string(String);
 
-    __declspec(dllimport) NativeErrorCode entity_to_string(EntityRef, StringOut);
-    __declspec(dllimport) NativeErrorCode entity_latexise(EntityRef, StringOut);
-    __declspec(dllimport) NativeErrorCode maths_from_string(String, EntityOut);
+    DLL_CODE NativeErrorCode entity_to_string(EntityRef, StringOut);
+    DLL_CODE NativeErrorCode entity_latexise(EntityRef, StringOut);
+    DLL_CODE NativeErrorCode maths_from_string(String, EntityOut);
 
-    __declspec(dllimport) NativeErrorCode entity_differentiate(EntityRef, EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_integrate(EntityRef, EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_limit(EntityRef, EntityRef, EntityRef, ApproachFrom, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_alternate(EntityRef, NativeArray*);
-    __declspec(dllimport) NativeErrorCode entity_simplify(EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_evaled(EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_inner_simplified(EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode entity_to_long(EntityRef, long*);
-    __declspec(dllimport) NativeErrorCode entity_to_rational(EntityRef, LongTuple*);
-    __declspec(dllimport) NativeErrorCode entity_to_double(EntityRef, double*);
-    __declspec(dllimport) NativeErrorCode entity_to_complex(EntityRef, DoubleTuple*);
+    DLL_CODE NativeErrorCode entity_differentiate(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode entity_integrate(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode entity_limit(EntityRef, EntityRef, EntityRef, ApproachFrom, EntityOut);
+    DLL_CODE NativeErrorCode entity_alternate(EntityRef, NativeArray*);
+    DLL_CODE NativeErrorCode entity_simplify(EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode entity_evaled(EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode entity_inner_simplified(EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode entity_to_long(EntityRef, long*);
+    DLL_CODE NativeErrorCode entity_to_rational(EntityRef, LongTuple*);
+    DLL_CODE NativeErrorCode entity_to_double(EntityRef, double*);
+    DLL_CODE NativeErrorCode entity_to_complex(EntityRef, DoubleTuple*);
 
-    __declspec(dllimport) NativeErrorCode op_entity_add(EntityRef, EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode op_entity_sub(EntityRef, EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode op_entity_mul(EntityRef, EntityRef, EntityOut);
-    __declspec(dllimport) NativeErrorCode op_entity_div(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode op_entity_add(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode op_entity_sub(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode op_entity_mul(EntityRef, EntityRef, EntityOut);
+    DLL_CODE NativeErrorCode op_entity_div(EntityRef, EntityRef, EntityOut);
 
-    __declspec(dllimport) NativeErrorCode entity_nodes(EntityRef, NativeArray*);
-    __declspec(dllimport) NativeErrorCode entity_vars(EntityRef, NativeArray*);
-    __declspec(dllimport) NativeErrorCode entity_vars_and_constants(EntityRef, NativeArray*);
+    DLL_CODE NativeErrorCode entity_nodes(EntityRef, NativeArray*);
+    DLL_CODE NativeErrorCode entity_vars(EntityRef, NativeArray*);
+    DLL_CODE NativeErrorCode entity_vars_and_constants(EntityRef, NativeArray*);
 }
