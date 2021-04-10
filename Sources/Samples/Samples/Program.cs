766b74d7fb11fce645d87667d09b80008a4b73f6
WhiteBlackGoose <wbg@angouri.org>
2021-04-10 20:03:50 +0300

    Decreased to 2000

--- Sources/Samples/Samples/Program.cs
+++ Sources/Samples/Samples/Program.cs
@@ -13,4 +13,6 @@ using System.Runtime.CompilerServices;
 
 // Console.WriteLine("[ [ 1, 2 ] ; [ 3, 4 ] ]".ToEntity());
 Matrix m = "[[3 - lambda, -1, 0, -2, 0], [-3, -4 - lambda, -2, 1, 3], [0, -7, 1 - lambda, -5, 2], [3, 4, 1, 1 - lambda, -2], [-6, -19, -5, -3, 10 - lambda]]";
-Console.WriteLine(m.Determinant.Expand().Simplify());
+Console.WriteLine(m.Determinant.Simplify());
+// using var _ = MathS.Settings.MaxExpansionTermCount.Set(50);
+// Console.WriteLine("(a + b)100".Expand());
