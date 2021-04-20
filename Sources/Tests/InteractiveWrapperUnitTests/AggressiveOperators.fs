c7d654fd733ccc2db385eea5d311c186ed3cd21e
WhiteBlackGoose <wbg@angouri.org>
2021-04-20 12:59:01 +0300

    Tests

---
+++ Sources/Tests/InteractiveWrapperUnitTests/AggressiveOperators.fs
@@ -0,0 +1,36 @@
+﻿module Tests.AggressiveOperators
+
+open Xunit
+open Functions
+open Core
+open AggressiveOperators
+
+[<Fact>]
+let ``Int / int is rational`` () =
+    let threeOverTwo = 3 / 2
+    Assert.IsAssignableFrom<AngouriMath.Entity.Number.Rational>(threeOverTwo) |> ignore
+    Assert.Equal(simplified (parsed "3 / 2"), threeOverTwo)
+
+[<Fact>]
+let ``Int + real is real`` () =
+    let num = 3 + 4.5
+    Assert.IsAssignableFrom<AngouriMath.Entity.Number.Rational>(num) |> ignore
+    Assert.Equal(simplified (parsed "3 + 4.5"), num)
+
+[<Fact>]
+let ``Int - real is real`` () =
+    let num = 3 - 4.5
+    Assert.IsAssignableFrom<AngouriMath.Entity.Number.Rational>(num) |> ignore
+    Assert.Equal(simplified (parsed "3 - 4.5"), num)
+
+[<Fact>]
+let ``Int * real is real`` () =
+    let num = 3 * 4.5
+    Assert.IsAssignableFrom<AngouriMath.Entity.Number.Rational>(num) |> ignore
+    Assert.Equal(simplified (parsed "3 * 4.5"), num)
+
+[<Fact>]
+let ``Int ** big number is real`` () =
+    let num = 3 ** "1000"
+    Assert.IsAssignableFrom<AngouriMath.Entity.Number.Integer>(num) |> ignore
+    Assert.Equal(simplified (parsed "3 ^ 1000"), num)
--- Sources/Wrappers/AngouriMath.Interactive/Main.fs
+++ Sources/Wrappers/AngouriMath.Interactive/Main.fs
@@ -5,9 +5,8 @@ open Microsoft.DotNet.Interactive.Formatting
 
 let magic() =
     let register (value : ILatexiseable) = $@"
-            <script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
-            <script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
-            \[{value.Latexise()}\]
-            "
+<script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
+<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
+\[{value.Latexise()}\]"
 
     Formatter.Register<ILatexiseable>(register, "text/html")
