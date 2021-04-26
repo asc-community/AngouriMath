25c3073a464cbf1c7a7ec51a5a81dae7bee64937
WhiteBlackGoose <wbg@angouri.org>
2021-04-26 16:52:43 +0300

    No unsafe flag in the playground

--- Sources/Samples/Samples/Program.cs
+++ Sources/Samples/Samples/Program.cs
@@ -8,25 +8,4 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
 
-Console.WriteLine("Stack ptr: " + LastStackPointer());
-MakeLambda()(12);
-
-static unsafe Action<int> MakeLambda()
-{
-    Console.WriteLine("Stack ptr: " + LastStackPointer());
-    var localVar = 0;
-    Action<int> res = a =>
-    {
-        localVar = a;
-        Console.WriteLine("Ptr: " + (nint)Unsafe.AsPointer(ref localVar));
-        Console.WriteLine("New value: " + localVar);
-    };
-    res(6);
-    return res;
-}
-
-static unsafe nint LastStackPointer()
-{
-    byte a = 0;
-    return (nint)Unsafe.AsPointer(ref a);
-}
+Console.WriteLine("Ducks.");
