75c7aa8deea34ddf369bb555c1a8b721d26ea7bc
WhiteBlackGoose <wbg@angouri.org>
2021-04-29 12:50:01 +0300

    Operators added; adding tests

---
+++ Sources/Tests/FSharpWrapperUnitTests/MatrixOperatorsTest.fs
@@ -0,0 +1,19 @@
+﻿module MatrixOperatorsTest
+
+open AngouriMath.FSharp.Core
+open AngouriMath.FSharp.Functions
+open AngouriMath.FSharp.MatrixOperators
+open Xunit
+open AngouriMath
+
+[<Fact>]
+let ``M-M *`` () =
+    Assert.Equal<Entity.Matrix>(vector [2; 4], "[[2, 0], [0, 2]]" |* "[1, 2]")
+
+[<Fact>]
+let ``M-S *`` () =
+    Assert.Equal<Entity.Matrix>(vector [3; 6], (vector [1; 3]) |* 3)
+
+[<Fact>]
+let ``S-M *`` () =
+    Assert.Equal<Entity.Matrix>(vector [3; 6], 3 |* (vector [1; 3]))
---
+++ Sources/Wrappers/AngouriMath.FSharp/MatricesOperators.fs
@@ -0,0 +1,30 @@
+﻿module AngouriMath.FSharp.MatrixOperators
+
+open AngouriMath
+open AngouriMath.FSharp.Core
+open AngouriMath.FSharp.Functions
+
+let (|*) a b =
+    match (parsed a * parsed b).InnerSimplified with
+    | :? Entity.Matrix as m -> m
+    | other -> vector [other]
+
+let (|**) a b =
+    match (parsed a ** parsed b).InnerSimplified with
+    | :? Entity.Matrix as m -> m
+    | other -> vector [other]
+
+let (|+) a b =
+    match (parsed a + parsed b).InnerSimplified with
+    | :? Entity.Matrix as m -> m
+    | other -> vector [other]
+
+let (|-) a b =
+    match (parsed a - parsed b).InnerSimplified with
+    | :? Entity.Matrix as m -> m
+    | other -> vector [other]
+
+let (|/) a b =
+    match (parsed a / parsed b).InnerSimplified with
+    | :? Entity.Matrix as m -> m
+    | other -> vector [other]
