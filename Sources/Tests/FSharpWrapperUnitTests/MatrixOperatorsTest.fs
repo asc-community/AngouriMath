fb38e96a033897c5c79272735a5e9005cc6fcedf
WhiteBlackGoose <wbg@angouri.org>
2021-04-30 22:26:55 +0300

    Tests fixed & added

--- Sources/Tests/FSharpWrapperUnitTests/MatrixOperatorsTest.fs
+++ Sources/Tests/FSharpWrapperUnitTests/MatrixOperatorsTest.fs
@@ -6,14 +6,59 @@ open AngouriMath.FSharp.MatrixOperators
 open Xunit
 open AngouriMath
 
+(*
+
+ Matrix and matrix operators
+
+*)
+
+[<Fact>]
+let ``M-M +`` () =
+    Assert.Equal<Entity.Matrix>(vector [11; 32], (vector [10; 30]) |+ (vector [1; 2]))
+
+[<Fact>]
+let ``M-M -`` () =
+    Assert.Equal<Entity.Matrix>(vector [9; 28], (vector [10; 30]) |- (vector [1; 2]))
+
 [<Fact>]
 let ``M-M *`` () =
     Assert.Equal<Entity.Matrix>(vector [2; 4], "[[2, 0], [0, 2]]" |* "[1, 2]")
 
+(*
+
+Matrix and scalar operators
+Scalar and matrix operators
+
+*)
+
+[<Fact>]
+let ``M-S +`` () =
+    Assert.Equal<Entity.Matrix>(vector [11; 31], (vector [10; 30]) |+ 1)
+
+[<Fact>]
+let ``S-M +`` () =
+    Assert.Equal<Entity.Matrix>(vector [11; 31], 1 |+ (vector [10; 30]))
+
+[<Fact>]
+let ``M-S -`` () =
+    Assert.Equal<Entity.Matrix>(vector [9; 29], (vector [10; 30]) |- 1)
+
+[<Fact>]
+let ``S-M -`` () =
+    Assert.Equal<Entity.Matrix>(vector [90; 70], 100 |- (vector [10; 30]))
+
 [<Fact>]
 let ``M-S *`` () =
-    Assert.Equal<Entity.Matrix>(vector [3; 6], (vector [1; 3]) |* 3)
+    Assert.Equal<Entity.Matrix>(vector [3; 6], (vector [1; 2]) |* 3)
 
 [<Fact>]
 let ``S-M *`` () =
-    Assert.Equal<Entity.Matrix>(vector [3; 6], 3 |* (vector [1; 3]))
+    Assert.Equal<Entity.Matrix>(vector [3; 6], 3 |* (vector [1; 2]))
+
+[<Fact>]
+let ``M-S /`` () =
+    Assert.Equal<Entity.Matrix>(vector [5; 15], (vector [10; 30]) |/ 2)
+
+[<Fact>]
+let ``M-S **`` () =
+    Assert.Equal<Entity.Matrix>(matrix [[37; 54]; [81; 118]], (matrix [[1; 2]; [3; 4]]) |** 3)
