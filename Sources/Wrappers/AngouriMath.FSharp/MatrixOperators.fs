d3e803c494a8e4b99b82ad3fb964d95e92ce1837
WhiteBlackGoose <wbg@angouri.org>
2021-05-18 10:59:53 +0300

    Tensor power for F# added (#418)
    1. `asMatrix` moved to Functions
    2. `****` and `***` both now apply `asMatrix` at the end
    3. `****` and `***` both are in functions
    4. Tests for both `****` and `***` added
    5. Info about packages added to readme

--- Sources/Wrappers/AngouriMath.FSharp/MatrixOperators.fs
+++ Sources/Wrappers/AngouriMath.FSharp/MatrixOperators.fs
@@ -4,14 +4,6 @@ open AngouriMath
 open AngouriMath.FSharp.Core
 open AngouriMath.FSharp.Functions
 
-/// If the provided entity is a matrix,
-/// it is returned downcasted. Otherwise,
-/// a 1x1 matrix containing the entity is returned.
-let asMatrix a = 
-    match parsed a with
-    | :? Entity.Matrix as m -> m
-    | other -> vector [other]
-
 let (|*) a b = (parsed a * parsed b).InnerSimplified |> asMatrix
 
 let (|**) a b = (parsed a ** parsed b).InnerSimplified |> asMatrix
