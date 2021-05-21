d1d2750ff11c0ce59557f1904a139a9e364e6f70
WhiteBlackGoose <wbg@angouri.org>
2021-05-21 13:23:50 +0300

    matrix 2x2, vector2, newMatrix

--- Sources/Wrappers/AngouriMath.FSharp/MatrixOperators.fs
+++ Sources/Wrappers/AngouriMath.FSharp/MatrixOperators.fs
@@ -13,3 +13,19 @@ let (|+) a b = (parsed a + parsed b).InnerSimplified |> asMatrix
 let (|-) a b = (parsed a - parsed b).InnerSimplified |> asMatrix
 
 let (|/) a b = (parsed a / parsed b).InnerSimplified |> asMatrix
+
+/// Creates a square 2x2 matrix, with four elements
+/// in the following order: left-top, right-top,
+/// left-bottom, right-bottom
+let matrix2x2 leftTop rightTop leftBottom rightBottom = 
+  matrix [[leftTop; rightTop]; [leftBottom; rightBottom]]
+
+/// Creates a vector of two elements
+let vector2 a b = vector [a; b]
+
+/// Creates a new matrix with each
+/// cell's coordinates mapped to
+/// an element (first comes the number of
+/// the row, then the number of column,
+/// starting from 0)
+let newMatrix rows cols map = MathS.Matrix(rows, cols, new System.Func<int, int, Entity>(map))
