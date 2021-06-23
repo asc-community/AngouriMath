c7aa5f00b1b319bb36113b7a57751f91d7fce75c
ryyanmapes <ryyanmapes@gmail.com>
2021-06-23 13:49:34 -0600

    Small spec fixes

--- Sources/Wrappers/AngouriMath.Interactive/AggressiveOperators.fs
+++ Sources/Wrappers/AngouriMath.Interactive/AggressiveOperators.fs
@@ -17,7 +17,7 @@ let ( ~~~ ) a =
     (negation (parsed a)).InnerSimplified
 let ( ||| ) a b = 
     (union (parsed a) (parsed b)).InnerSimplified
-let ( ^^^ ) a b = 
+let ( &&& ) a b = 
     (intersect (parsed a) (parsed b)).InnerSimplified
 let ( = ) a b = 
     (equality (parsed a) (parsed b)).InnerSimplified
@@ -31,29 +31,3 @@ let ( >= ) a b =
     (greaterOrEqual (parsed a) (parsed b)).InnerSimplified  
 let ( <= ) a b = 
     (lessOrEqual (parsed a) (parsed b)).InnerSimplified
-let sin a = 
-    (sin (parsed a)).InnerSimplified
-let cos a = 
-    (cos (parsed a)).InnerSimplified
-let tan a = 
-    (tan (parsed a)).InnerSimplified
-let asin a = 
-    (asin (parsed a)).InnerSimplified
-let acos a = 
-    (acos (parsed a)).InnerSimplified
-let atan a = 
-    (atan (parsed a)).InnerSimplified
-let sinh a = 
-    (sinh (parsed a)).InnerSimplified
-let cosh a = 
-    (cosh (parsed a)).InnerSimplified
-let tanh a = 
-    (tanh (parsed a)).InnerSimplified
-let sqrt a = 
-    (sqrt (parsed a)).InnerSimplified
-let log logbase a = 
-    (log (parsed logbase) (parsed a)).InnerSimplified
-let log10 a = 
-    (log (parsed "10") (parsed a)).InnerSimplified
-let ln a = 
-    (ln (parsed a)).InnerSimplified
