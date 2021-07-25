3aaf1ba9629e8d4892a73eec83b83fe0733dd0f1
WhiteBlackGoose <wbg@angouri.org>
2021-07-25 08:31:25 +0300

    formatters for PeterONumbers added and Honk# updated

--- Sources/Wrappers/AngouriMath.Interactive/KernelExtension.fs
+++ Sources/Wrappers/AngouriMath.Interactive/KernelExtension.fs
@@ -4,16 +4,29 @@ open Microsoft.DotNet.Interactive
 open Microsoft.DotNet.Interactive.Formatting
 open AngouriMath.Core
 open System.Threading.Tasks
+open PeterO.Numbers
+open System
 
 type KernelExtension() = 
     static member public applyMagic () =
-        let register (value : ILatexiseable) = $@"
-<script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
-<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
-\[{value.Latexise()}\]"
+        let latexWrap latex = $@"
+            <script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
+            <script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
+            \[{latex}\]"
+
+        let register (value : ILatexiseable) = value.Latexise() |> latexWrap
         
         Formatter.Register<ILatexiseable>(register, "text/html")
         Formatter.SetPreferredMimeTypeFor(typeof<ILatexiseable>, "text/html")
+
+        Formatter.SetPreferredMimeTypeFor(typeof<EDecimal>, "text/plain")
+        Formatter.SetPreferredMimeTypeFor(typeof<EInteger>, "text/plain")
+
+        Formatter.SetPreferredMimeTypeFor(typeof<ERational>, "text/html")
+        Formatter.Register<ERational>(
+            new Func<ERational, string>(fun o -> latexWrap $@"\frac{o.Numerator}{o.Denominator}"), "text/html")
+
+
 
 
     interface IKernelExtension with
