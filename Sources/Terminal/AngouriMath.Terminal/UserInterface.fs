c6e87d81d0e0fbdf43b04dc8a80cbc4de5bffc81
WhiteBlackGoose <wbg@angouri.org>
2021-07-30 21:34:56 +0300

    Functions from am to be recognized

--- Sources/Terminal/AngouriMath.Terminal/UserInterface.fs
+++ Sources/Terminal/AngouriMath.Terminal/UserInterface.fs
@@ -35,7 +35,16 @@ let writeLineError (ansiConsole : IAnsiConsole) (input : string) =
     |> ansiConsole.Write
 
     
-let getWordHighlighter () =
+let private getWordsFromAngouriMathFSharp () =
+    typeof<AngouriMath.FSharp.Core.ParseException>.Assembly.GetTypes()
+    |> Seq.collect (fun t -> t.GetMethods ())
+    |> Seq.map (fun method -> method.Name)
+    |> Seq.filter (fun name -> name.Contains "_" |> not)
+    |> Seq.filter (fun name -> name.[0] = name.ToLower().[0])  // must not be PascalCase
+    |> List.ofSeq
+
+
+let private getWordHighlighter () =
     let rec addWord words style (highlighter : WordHighlighter) =
         match words with
         | hd::tl -> 
@@ -45,13 +54,18 @@ let getWordHighlighter () =
 
     let highlighter = WordHighlighter ()
 
-    let keywordStyle = new Style(Color.LightSlateBlue);
+    let keywords = ["let"; "int"; "bool"; "for"; "mutable"; "null"; "match"; "with"; "fun"]
+    let operators = ["("; ")"; "|>"; ":"; "["; "]"]
+    let digits = ["0"; "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "."; "-"]
+    let amFunctions = getWordsFromAngouriMathFSharp ()
 
-    let keywords = ["let"; "int"]
-
-    addWord keywords keywordStyle highlighter
+    addWord keywords (Style Color.LightSlateBlue) highlighter
+    addWord operators (Style Color.Pink1) highlighter
+    addWord digits (Style Color.Pink3) highlighter
+    addWord amFunctions (Style Color.Gold3_1) highlighter
 
     highlighter
+
 
 type TimePrefixEditorPromt() =
     interface ILineEditorPrompt with
