147cd62d7d9b18edcee17685351d7704a55f28ca
WhiteBlackGoose <wbg@angouri.org>
2021-08-04 09:55:56 +0300

    2D plotting added

--- Sources/Wrappers/AngouriMath.Interactive/Graphs.fs
+++ Sources/Wrappers/AngouriMath.Interactive/Graphs.fs
@@ -6,6 +6,7 @@ open AngouriMath.FSharp.Core
 
 exception InvalidInput
 
+
 let displayFunc (range : 'T seq) (func : obj) =
     let entity = parsed func
     let vars = entity.Vars |> List.ofSeq
@@ -15,4 +16,20 @@ let displayFunc (range : 'T seq) (func : obj) =
         let xData = List.ofSeq range
         let yData = List.map compiled xData
         Chart.Point (xData, yData)
+    | _ -> raise InvalidInput
+
+
+let displayFunc2D (xRange : 'T1 seq) (yRange : 'T2 seq) (func : obj) =
+    let entity = parsed func
+    let vars = entity.Vars |> List.ofSeq
+    match vars with
+    | firstVar::tail ->
+        match tail with
+        | secondVar::[] ->
+            let compiled = compiled2In<'T1, 'T2, double> firstVar secondVar func
+            let xData = List.ofSeq xRange
+            let yData = List.ofSeq yRange
+            let zData = List.map (fun x -> List.map (fun y -> compiled x y) yData) xData
+            Chart.Surface (zData, xData, yData)
+        | _ -> raise InvalidInput
     | _ -> raise InvalidInput
