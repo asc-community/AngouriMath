064892980944750bc9c14d35a0148c00ad1eeebb
WhiteBlackGoose <wbg@angouri.org>
2021-09-09 20:29:24 +0300

    Reverted for .net foundation license

--- Sources/AngouriMath/RequiresPreviewFeaturesAttribute.cs
+++ Sources/AngouriMath/RequiresPreviewFeaturesAttribute.cs
@@ -1,11 +1,10 @@
-﻿//
-// Copyright (c) 2019-2021 Angouri.
-// AngouriMath is licensed under MIT.
-// Details: https://github.com/asc-community/AngouriMath/blob/master/LICENSE.md.
-// Website: https://am.angouri.org.
-//
+﻿#pragma warning disable IDE0073 // The file header does not match the required text
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#pragma warning restore IDE0073 // The file header does not match the required text
 
 namespace System.Runtime.Versioning
+
 {
     /// Yes.
     [AttributeUsage(AttributeTargets.Assembly |
