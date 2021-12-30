7ccd556f9d4c164aea967a27165e1c73e4fbc204
WhiteBlackGoose <wbg@angouri.org>
2021-12-30 21:18:20 +0300

    Working on general things for slider

--- Sources/Wrappers/AngouriMath.Interactive/Plot.fs
+++ Sources/Wrappers/AngouriMath.Interactive/Plot.fs
@@ -25,7 +25,7 @@ let span start finish =
     } |> List.ofSeq
 
 
-let diagnonalTraverse (seq1 : list<'T1>) (seq2 : list<'T2>) =
+let diagnonalTraverse (seq1 : list<double>) (seq2 : list<double>) =
     let arr1 = Array.ofSeq seq1
     let arr2 = Array.ofSeq seq2
     let n = arr1.Length
@@ -56,47 +56,68 @@ let private getOnlyTwoVariables (func : AngouriMath.Entity) =
     | [] -> symbol "x", symbol "y" // any variables are ok
     | [ oneVar ] -> oneVar, symbol ("t" + oneVar.Name)
     | [ firstVar; secondVar ] -> firstVar, secondVar
-    | _ -> raise (InvalidInput $"There should be exactly one variable, found: {vars}")
+    | _ -> raise (InvalidInput $"There should be exactly two variables, found: {vars}")
+
+let private getOnlyThreeVariables (func : AngouriMath.Entity) =
+    let vars = getVarList func
+    match vars with
+    | [] -> symbol "x", symbol "y", symbol "z"     // any variables are ok
+    | [ oneVar ] -> oneVar, symbol ("t" + oneVar.Name), symbol ("tt" + oneVar.Name)
+    | [ firstVar; secondVar ] -> firstVar, secondVar, symbol ("tt" + (if firstVar.Name.Length > secondVar.Name.Length then firstVar else secondVar).Name)
+    | [ firstVar; secondVar; thirdVar ] -> firstVar, secondVar, thirdVar
+    | _ -> raise (InvalidInput $"There should be exactly three variables, found: {vars}")
 
 
-let private prepareLinearData (range : 'T seq) (func : obj) =
-    let entity = parsed func
-    let theOnlyVar = getSingleVariable entity
-    let compiled = compiled1In<'T, double> theOnlyVar func
+let private compile1 (func : obj) =
+    match func with
+    | :? (double -> double) as res -> res
+    | _  ->
+        let var = getSingleVariable (parsed func)
+        compiled1In<double, double> var func
+
+let private compile2 (func : obj) =
+    match func with
+    | :? (double * double -> double) as res -> res
+    | _ ->
+        let (var1, var2) = getOnlyTwoVariables (parsed func)
+        let f = compiled2In<double, double, double> var1 var2 func
+        (fun (x, y) -> f x y)
+
+let private prepareLinearData (range : double seq) (func : double -> double) =
     let xData = List.ofSeq range
-    let yData = List.map compiled xData
+    let yData = List.map func xData
     (xData, yData)
 
-let private preparePolarData (range: 'T seq) (func : obj) =
+let private preparePolarData (range: double seq) (func : double -> double) =
     let (phiData, rData) = prepareLinearData range func
     let polar = List.zip phiData rData
     let (phi, r) = (symbol "phi", symbol "r")
-    let compiledX = compiled2In<'T, double, double> phi r "r * cos(phi)"
-    let compiledY = compiled2In<'T, double, double> phi r "r * sin(phi)"
+    let compiledX = compiled2In<double, double, double> phi r "r * cos(phi)"
+    let compiledY = compiled2In<double, double, double> phi r "r * sin(phi)"
     let xData = List.map (fun (phi, r) -> compiledX phi r) polar
     let yData = List.map (fun (phi, r) -> compiledY phi r) polar
     (xData, yData)
 
-let private prepareSurface3DData (xRange : 'T1 seq) (yRange : 'T2 seq) (func : obj) =
+let private prepareSurface3DData (xRange : double seq) (yRange : double seq) (func : (double * double) -> double) =
     let entity = parsed func
     let (firstVar, secondVar) = getOnlyTwoVariables entity
-    let compiled = compiled2In<'T1, 'T2, double> firstVar secondVar func
+    
     let xData = List.ofSeq xRange
     let yData = List.ofSeq yRange
-    let zData = List.map (fun x -> List.map (fun y -> compiled x y) yData) xData
+    let zData = List.map (fun x -> List.map (fun y -> func(x, y)) yData) xData
     (zData, xData, yData)
         
 
-let private getPolarToRegular3D<'T> () =
-    compiled3In<'T, 'T, double, double> "phi_1" "phi_2" "r" "r * sin(phi_2)",
-    compiled3In<'T, 'T, double, double> "phi_1" "phi_2" "r" "r * sin(phi_1) * cos(phi_2)",
-    compiled3In<'T, 'T, double, double> "phi_1" "phi_2" "r" "r * cos(phi_1) * cos(phi_2)"
+let private getPolarToRegular3D () =
+    compiled3In<double, double, double, double> "phi_1" "phi_2" "r" "r * sin(phi_2)",
+    compiled3In<double, double, double, double> "phi_1" "phi_2" "r" "r * sin(phi_1) * cos(phi_2)",
+    compiled3In<double, double, double, double> "phi_1" "phi_2" "r" "r * cos(phi_1) * cos(phi_2)"
 
 
-let private prepareQuadraticData (xRange : 'T1 seq) (yRange : 'T2 seq) (func : obj) traverse =
+let private prepareQuadraticData (xRange : double seq) (yRange : double seq) (func : obj) traverse =
     let entity = parsed func
     let (firstVar, secondVar) = getOnlyTwoVariables entity
-    let compiled = compiled2In<'T1, 'T2, double> firstVar secondVar func
+    let compiled = compiled2In<double, double, double> firstVar secondVar func
     let xDataRaw = List.ofSeq xRange
     let yDataRaw = List.ofSeq yRange
     let xy = traverse xDataRaw yDataRaw
@@ -106,9 +127,9 @@ let private prepareQuadraticData (xRange : 'T1 seq) (yRange : 'T2 seq) (func : o
     (zData, xData, yData)
 
 
-let private preparePolarSurfaceDataFromAngles (phi1Range : 'T seq) (phi2Range : 'T seq) (func : obj) =
+let private preparePolarSurfaceDataFromAngles (phi1Range : double seq) (phi2Range : double seq) (func : obj) =
     let (rData, phi1Data, phi2Data) = prepareQuadraticData phi1Range phi2Range func List.allPairs
-    let (xOfPoint, yOfPoint, zOfPoint) = getPolarToRegular3D<'T> ()
+    let (xOfPoint, yOfPoint, zOfPoint) = getPolarToRegular3D ()
 
     let data =
         (rData, phi1Data, phi2Data)
@@ -127,10 +148,10 @@ let private preparePolarSurfaceDataFromAngles (phi1Range : 'T seq) (phi2Range : 
     // let z = List.map (fun (r, p1, p2) -> List.map (fun (r, p1, p2) -> zOfPoint p1 p2 r) data) data
     (z, x, y)
         
-let private preparePolarScatter3DFromAngles (phi1Range : 'T seq) (phi2Range : 'T seq) (func : obj) =
+let private preparePolarScatter3DFromAngles (phi1Range : double seq) (phi2Range : double seq) (func : obj) =
     let (rData, phi1Data, phi2Data) = prepareQuadraticData phi1Range phi2Range func diagnonalTraverse
 
-    let (xOfPoint, yOfPoint, zOfPoint) = getPolarToRegular3D<'T> ()
+    let (xOfPoint, yOfPoint, zOfPoint) = getPolarToRegular3D ()
 
     List.zip3 phi1Data phi2Data rData
     |> List.map (fun (phi1, phi2, r) ->
@@ -167,36 +188,72 @@ let private getSlider (range : seq<float>) =
         Steps=sliderSteps
     )
 
-let linear (range : 'T seq) (func : obj) =
-    let (xData, yData) = prepareLinearData range func
-    Chart.Line (xData, yData)
-    |> withTransparency
+let private castAs<'a, 'b> (a : 'a) : 'b =
+    let up : obj = a :> obj
+    up :?> 'b
 
-let linearWithSlider (range : float seq) (func : obj) (a : obj) (sliderRange : float seq) =
-    let (v1, v2) = getOnlyTwoVariables (parsed func)
-    let x = if v1 = symbol a then v2 else v1
-    let compiled = compiled2In<float, float, float> x a func
-
+let private withSliderND n (chartPlotter : 'a -> obj -> GenericChart.GenericChart) (ranges : 'a) (paramRange : double seq) (param : obj) (func : obj) =
+    let var = symbol param
+    let compiled =
+        match n with
+        | 1 ->
+            let (v1, v2) = getOnlyTwoVariables (parsed func)
+            let x = if v1 = var then v2 else v1
+            let f = compiled2In<double, double, double> var x func
+            castAs<_, double -> 'a -> double> f
+        | 2 ->
+            let (v1, v2, v3) = getOnlyThreeVariables (parsed func)
+            let (x, y) =
+                if var = v1 then (v2, v3)
+                else if var = v2 then (v1, v3)
+                else (v1, v2)
+            let f = compiled3In<double, double, double, double> var x y func
+            castAs<_, double -> 'a -> double> f
+        | _ -> raise (System.Exception $"Can't have {n}-dimensional slider")
     let charts = 
-        sliderRange
+        paramRange
         |> Seq.map (fun step ->
-            let (xData, yData) = range |> Seq.map (fun x -> x, compiled x step) |> Seq.unzip
-            Chart.Line (xData, yData)
-            |> Chart.withTraceName(Visible= if step = Seq.head sliderRange then StyleParam.Visible.True else StyleParam.Visible.False)
+            let newFunc = compiled step
+            chartPlotter ranges newFunc
+            |> Chart.withTraceName(Visible = if step = Seq.head paramRange then StyleParam.Visible.True else StyleParam.Visible.False)
         )
         |> GenericChart.combine
-    let slider = getSlider sliderRange
+    let slider = getSlider paramRange
+
+    charts
+    |> Chart.withSlider slider
+
+let withSlider1D (chartPlotter : double seq -> obj -> GenericChart.GenericChart) (range : double seq) (paramRange : double seq) (param : obj) (func : obj) =
+    let var = symbol param
+    let (v1, v2) = getOnlyTwoVariables (parsed func)
+    let x = if v1 = var then v2 else v1
+    let compiled = compiled2In<double, double, double> var x func
+    let charts = 
+        paramRange
+        |> Seq.map (fun step ->
+            let newFunc = compiled step
+            chartPlotter range newFunc
+            |> Chart.withTraceName(Visible = if step = Seq.head paramRange then StyleParam.Visible.True else StyleParam.Visible.False)
+        )
+        |> GenericChart.combine
+    let slider = getSlider paramRange
     charts
     |> Chart.withSlider slider
 
 
-let polarLinear (range : 'T seq) (func : obj) =
-    let (xData, yData) = preparePolarData range func
+let linear (range : double seq) (func : obj) =
+    let (xData, yData) = prepareLinearData range (compile1 func)
+    Chart.Line (xData, yData)
+    |> withTransparency
+
+
+let polarLinear (range : double seq) (func : obj) =
+    let (xData, yData) = preparePolarData range (compile1 func)
     Chart.Line (xData, yData)
     |> withTransparency
    
-let polarScatter2D (range : 'T seq) (func : obj) =
-    let (xData, yData) = preparePolarData range func
+let polarScatter2D (range : double seq) (func : obj) =
+    let (xData, yData) = preparePolarData range (compile1 func)
     Chart.Point (xData, yData)
     |> withTransparency
 
@@ -225,7 +282,7 @@ let colorPoints3D points colorA colorB =
         else Color.fromKeyword ColorKeyword.Gray)
 
     
-let sphericalScatter3D (phi1Range : 'T seq) (phi2Range : 'T seq) (func : obj) =
+let sphericalScatter3D (phi1Range : double seq) (phi2Range : double seq) (func : obj) =
     let points = preparePolarScatter3DFromAngles phi1Range phi2Range func
     let zipped = points |||> List.zip3
     let colors = colorPoints3D zipped (255, 75, 75) (75, 75, 255)
@@ -233,24 +290,24 @@ let sphericalScatter3D (phi1Range : 'T seq) (phi2Range : 'T seq) (func : obj) =
     |> (fun l -> Chart.Point3D(l, MarkerColor = Color.fromColors colors) )
     |> withTransparency
 
-let sphericalSurface (phi1Range : 'T seq) (phi2Range : 'T seq) (func : obj) =
+let sphericalSurface (phi1Range : double seq) (phi2Range : double seq) (func : obj) =
     // preparePolarSurfaceDataFromAngles phi1Range phi2Range func
     // |> (fun (z, x, y) -> Chart.Surface(z, x, y))
     preparePolarScatter3DFromAngles phi1Range phi2Range func
     |> (fun (x, y, z) -> Chart.Mesh3D(x, y, z))
     |> withTransparency
 
-let scatter2D (range : 'T seq) (func : obj) =
-    let (xData, yData) = prepareLinearData range func
+let scatter2D (range : double seq) (func : obj) =
+    let (xData, yData) = prepareLinearData range (compile1 func)
     Chart.Point (xData, yData)
     |> withTransparency
 
-let surface (xRange : 'T1 seq) (yRange : 'T2 seq) (func : obj) =
-    let (zData, xData, yData) = prepareSurface3DData xRange yRange func
+let surface (xRange : double seq) (yRange : double seq) (func : obj) =
+    let (zData, xData, yData) = prepareSurface3DData xRange yRange (compile2 func)
     Chart.Surface (zData, xData, yData)
     |> withTransparency
 
-let scatter3D (xRange : 'T1 seq) (yRange : 'T2 seq) (func : obj) =
+let scatter3D (xRange : double seq) (yRange : double seq) (func : obj) =
     prepareQuadraticData xRange yRange func List.allPairs
     |||> List.zip3
     |> Chart.Point3D
