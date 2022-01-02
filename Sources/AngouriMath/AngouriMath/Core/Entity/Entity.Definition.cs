f457fcf60756a0bb60a18ebe683085ed12e27b9b
Bimal parajuli <65300475+bimal-parajuli@users.noreply.github.com>
2022-01-03 01:57:21 +0545

    Update Copyright information from 2021 to 2022 (#555)
    * Update Copyright information from 2021 to 2022
    
    * Update copyright year in all files including .editorconfig file

--- Sources/AngouriMath/AngouriMath/Core/Entity/Entity.Definition.cs
+++ Sources/AngouriMath/AngouriMath/Core/Entity/Entity.Definition.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2019-2021 Angouri.
+// Copyright (c) 2019-2022 Angouri.
 // AngouriMath is licensed under MIT.
 // Details: https://github.com/asc-community/AngouriMath/blob/master/LICENSE.md.
 // Website: https://am.angouri.org.
