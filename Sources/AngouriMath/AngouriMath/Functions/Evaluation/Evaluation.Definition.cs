7d81f3abfe8ca6f678bb7213835b88f864efd5f2
WhiteBlackGoose <wbg@angouri.org>
2022-07-20 12:37:10 +0300

    Docs...

--- Sources/AngouriMath/AngouriMath/Functions/Evaluation/Evaluation.Definition.cs
+++ Sources/AngouriMath/AngouriMath/Functions/Evaluation/Evaluation.Definition.cs
@@ -24,6 +24,24 @@ namespace AngouriMath
         /// consistency's sake, consider the call of this property
         /// as free as the addressing of a field.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var x = Var("x");
+        /// var expr = Sqr(Sin(x + 0)) + Sqr(Cos(x / 1));
+        /// Console.WriteLine(expr);
+        /// Console.WriteLine(expr.InnerSimplified);
+        /// Console.WriteLine(expr.Simplify());
+        /// </code>
+        /// Prints
+        /// <code>
+        /// sin(x + 0) ^ 2 + cos(x / 1) ^ 2
+        /// sin(x) ^ 2 + cos(x) ^ 2
+        /// 1
+        /// </code>
+        /// </example>
         public Entity InnerSimplified => innerSimplified.GetValue(static @this => @this.InnerSimplifyWithCheck(), this);
         private LazyPropertyA<Entity> innerSimplified;
 
@@ -69,6 +87,30 @@ namespace AngouriMath
         /// current entity otherwise
         /// To change the limit use <see cref="MathS.Settings.MaxExpansionTermCount"/>
         /// </returns>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y) = Var("x", "y");
+        /// 
+        /// var expr = (x + 3) * (Sin(y) + 5);
+        /// Console.WriteLine(expr);
+        /// Console.WriteLine(expr.Expand());
+        /// Console.WriteLine("-----------------------------------");
+        /// var expr2 = Pow(x + y, 8);
+        /// Console.WriteLine(expr2);
+        /// Console.WriteLine(expr2.Expand());
+        /// </code>
+        /// Prints
+        /// <code>
+        /// (x + 3) * (sin(y) + 5)
+        /// x * sin(y) + x * 5 + 3 * sin(y) + 15
+        /// -----------------------------------
+        /// (x + y) ^ 8
+        /// y ^ 8 + 8 * x * y ^ 7 + 28 * x ^ 2 * y ^ 6 + 56 * x ^ 3 * y ^ 5 + 70 * x ^ 4 * y ^ 4 + 56 * x ^ 5 * y ^ 3 + 28 * x ^ 6 * y ^ 2 + 8 * x ^ 7 * y + x ^ 8
+        /// </code>
+        /// </example> 
         public Entity Expand(int level = 2)
         {
             static Entity Expand_(Entity e, int level) =>
@@ -90,6 +132,30 @@ namespace AngouriMath
         /// <param name="level">
         /// The number of iterations (increase this argument if some factor operations are still available)
         /// </param>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y) = Var("x", "y");
+        /// 
+        /// var expr1 = x * y + y + x + 1;
+        /// Console.WriteLine(expr1);
+        /// Console.WriteLine(expr1.Factorize());
+        /// Console.WriteLine("-----------------------------------");
+        /// var expr2 = x * y + y + (1 + x);
+        /// Console.WriteLine(expr2);
+        /// Console.WriteLine(expr2.Factorize());
+        /// </code>
+        /// Prints
+        /// <code>
+        /// x * y + y + x + 1
+        /// y * (1 + x) + x + 1
+        /// -----------------------------------
+        /// x * y + y + 1 + x
+        /// (1 + x) * (1 + y)
+        /// </code>
+        /// </example>
         public Entity Factorize(int level = 2) => level <= 1
             ? this.Replace(Patterns.FactorizeRules)
             : this.Replace(Patterns.FactorizeRules).Factorize(level - 1);
@@ -100,10 +166,73 @@ namespace AngouriMath
         /// <param name="level">
         /// Increase this argument if you think the equation should be simplified better
         /// </param>
-        /// <returns></returns>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y, a) = Var("x", "y", "a");
+        /// var expr = Sin(x) + y + a;
+        /// Console.WriteLine(expr);
+        /// Console.WriteLine(expr.Simplify());
+        /// Console.WriteLine("---------------------");
+        /// var expr1 = Sin(x - 3) / Tan(x - 3) + Sec(Sqrt(y)) * Cosec(Sqrt(y));
+        /// Console.WriteLine(expr1);
+        /// Console.WriteLine(expr1.Simplify());
+        /// Console.WriteLine("---------------------");
+        /// var expr2 = Sin(pi / 3) * 2;
+        /// Console.WriteLine(expr2);
+        /// Console.WriteLine(expr2.Simplify());
+        /// Console.WriteLine("---------------------");
+        /// var expr3 = (Pow(x, 3) + 3 * Sqr(x) * y + 3 * x * Sqr(y) + Pow(y, 3)) / (x + y);
+        /// Console.WriteLine(expr3);
+        /// Console.WriteLine(expr3.Simplify());
+        /// Console.WriteLine("---------------------");
+        /// var expr4 = Derivative(Sin(Sqr(x * y) + y * x), x);
+        /// Console.WriteLine(expr4);
+        /// Console.WriteLine(expr4.Simplify());
+        /// </code>
+        /// Prints
+        /// <code>
+        /// sin(x) + y + a
+        /// sin(x) + a + y
+        /// ---------------------
+        /// sin(x - 3) / tan(x - 3) + sec(sqrt(y)) * csc(sqrt(y))
+        /// 2 * csc(2 * sqrt(y)) + cos(x - 3)
+        /// ---------------------
+        /// sin(pi / 3) * 2
+        /// sqrt(3)
+        /// ---------------------
+        /// (x ^ 3 + 3 * x ^ 2 * y + 3 * x * y ^ 2 + y ^ 3) / (x + y)
+        /// x ^ 2 + 2 * x * y + y ^ 2
+        /// ---------------------
+        /// derivative(sin((x * y) ^ 2 + y * x), x)
+        /// cos((x * y) ^ 2 + x * y) * (2 * x * y ^ 2 + y)
+        /// </code>
+        /// </example>
         public Entity Simplify(int level = 2) => Simplificator.Simplify(this, level);
 
         /// <summary>Finds all alternative forms of an expression sorted by their complexity</summary>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y, a) = Var("x", "y", "a");
+        /// var expr = x * y + y + x + x / y + a * (x + y);
+        /// foreach (var alt in expr.Alternate(level: 3))
+        ///     Console.WriteLine(alt);
+        /// </code>
+        /// <code>
+        /// a * y + (1 + a + 1 / y + y) * x + y
+        /// a * (x + y) + x + x * (y + 1 / y) + y
+        /// a * (x + y) + x + x * (1 / y + y) + y
+        /// x * y + y + x + x / y + a * (x + y)
+        /// a * (x + y) + x + x * y + x / y + y
+        /// (x + y) * a + x + x * y + x / y + y
+        /// a * (x + y) + x + x * 1 / y + x * y + y
+        /// </code>
+        /// </example>
         public IEnumerable<Entity> Alternate(int level) => Simplificator.Alternate(this, level);
 
         /// <summary>
@@ -117,12 +246,83 @@ namespace AngouriMath
         /// consistency's sake, consider the call of this property
         /// as free as the addressing of a field.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y) = Var("x", "y");
+        /// var expr1 = x + y;
+        /// Console.WriteLine(expr1);
+        /// Console.WriteLine(expr1.Evaled);
+        /// Console.WriteLine(expr1.Evaled.GetType());
+        /// Console.WriteLine("-----------------------------");
+        /// var expr2 = 5 + x * i;
+        /// Console.WriteLine(expr2);
+        /// Console.WriteLine(expr2.Evaled);
+        /// Console.WriteLine(expr2.Substitute(x, 3).Evaled);
+        /// Console.WriteLine(expr2.Substitute(x, 3).Evaled.GetType());
+        /// Console.WriteLine("-----------------------------");
+        /// var expr3 = GreaterThan(5, 3);
+        /// Console.WriteLine(expr3);
+        /// Console.WriteLine(expr3.Evaled);
+        /// Console.WriteLine(expr3.Evaled.GetType());
+        /// </code>
+        /// Prints
+        /// <code>
+        /// x + y
+        /// x + y
+        /// AngouriMath.Entity+Sumf
+        /// -----------------------------
+        /// 5 + x * i
+        /// 5 + x * i
+        /// 5 + 3i
+        /// AngouriMath.Entity+Number+Complex
+        /// -----------------------------
+        /// 5 > 3
+        /// True
+        /// AngouriMath.Entity+Boolean
+        /// </code>
+        /// </example>
         public Entity Evaled => evaled.GetValue(static @this => @this.InnerEvalWithCheck(), this);
         private LazyPropertyA<Entity> evaled;
 
         /// <summary>
         /// Determines whether a given element can be unambiguously used as a number or boolean
         /// </summary>
+        /// <example>
+        /// <code>
+        /// using System;
+        /// using static AngouriMath.MathS;
+        /// 
+        /// var (x, y) = Var("x", "y");
+        /// var expr1 = x + y;
+        /// Console.WriteLine(expr1.IsConstant);
+        /// Console.WriteLine(expr1.Evaled.IsConstant);
+        /// Console.WriteLine("-----------------------------");
+        /// var expr2 = 5 + x * i;
+        /// Console.WriteLine(expr2.IsConstant);
+        /// Console.WriteLine(expr2.Substitute(x, 3).IsConstant);
+        /// Console.WriteLine("-----------------------------");
+        /// var expr3 = GreaterThan(5, 3);
+        /// Console.WriteLine(expr3.IsConstant);
+        /// Console.WriteLine("-----------------------------");
+        /// var expr4 = pi + 0 * e;
+        /// Console.WriteLine(expr4.IsConstant);
+        /// </code>
+        /// Prints
+        /// <code>
+        /// False
+        /// False
+        /// -----------------------------
+        /// False
+        /// True
+        /// -----------------------------
+        /// True
+        /// -----------------------------
+        /// True
+        /// </code>
+        /// </example>
         public bool IsConstant => Evaled is Number.Complex or Boolean || Evaled is Variable v && Variable.ConstantList.ContainsKey(v);
     }
 }
--- Sources/Samples/Samples/Program.cs
+++ Sources/Samples/Samples/Program.cs
@@ -6,22 +6,19 @@
 //
 
 using System;
-using AngouriMath;
 using static AngouriMath.MathS;
 
-Entity expr1 = 5;
-Console.WriteLine($"{expr1}, IsConstantLeaf: {expr1.IsConstantLeaf}");
-Entity expr2 = Sin(5);
-Console.WriteLine($"{expr2}, IsConstantLeaf: {expr2.IsConstantLeaf}");
-Entity expr3 = pi;
-Console.WriteLine($"{expr3}, IsConstantLeaf: {expr3.IsConstantLeaf}");
-Entity expr4 = 3 + 4 * i;
-Console.WriteLine($"{expr4}, IsConstantLeaf: {expr4.IsConstantLeaf}");
-Entity expr5 = (Entity)3 + 4 * i;
-Console.WriteLine($"{expr5}, IsConstantLeaf: {expr5.IsConstantLeaf}");
-var expr6 = expr5.InnerSimplified;
-Console.WriteLine($"{expr6}, IsConstantLeaf: {expr6.IsConstantLeaf}");
-var expr7 = GreaterThan(pi, e);
-Console.WriteLine($"{expr7}, IsConstantLeaf: {expr7.IsConstantLeaf}");
-var expr8 = expr7.Evaled;
-Console.WriteLine($"{expr8}, IsConstantLeaf: {expr8.IsConstantLeaf}");
+var (x, y) = Var("x", "y");
+var expr1 = x + y;
+Console.WriteLine(expr1.IsConstant);
+Console.WriteLine(expr1.Evaled.IsConstant);
+Console.WriteLine("-----------------------------");
+var expr2 = 5 + x * i;
+Console.WriteLine(expr2.IsConstant);
+Console.WriteLine(expr2.Substitute(x, 3).IsConstant);
+Console.WriteLine("-----------------------------");
+var expr3 = GreaterThan(5, 3);
+Console.WriteLine(expr3.IsConstant);
+Console.WriteLine("-----------------------------");
+var expr4 = pi + 0 * e;
+Console.WriteLine(expr4.IsConstant);
