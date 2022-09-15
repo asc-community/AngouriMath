f27c87c8f39ae3e3bc3e91c86c25c78c767c5bd9
WhiteBlackGoose <wbg@angouri.org>
2022-09-15 22:51:52 +0300

    unifying publishing, adding ARM and 32-bit

--- Sources/Terminal/AngouriMath.Terminal/publish-osx-x64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dotnet publish \
--r osx-x64 \
--c release \
--o ./publish-output/osx-amd64 \
---self-contained
--- Sources/Terminal/AngouriMath.Terminal/publish-win-x64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dotnet publish \
--r win-x64 \
--c release \
--o ./publish-output/win-amd64 \
---self-contained
