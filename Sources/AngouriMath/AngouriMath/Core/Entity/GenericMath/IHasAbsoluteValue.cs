edab63ca643b6d84a76a4a65a53cadc14c7739d5
WhiteBlackGoose <wbg@angouri.org>
2022-11-28 19:29:59 +0300

    minor

--- Sources/AngouriMath/AngouriMath/Core/Entity/GenericMath/IHasAbsoluteValue.cs
+++ Sources/AngouriMath/AngouriMath/Core/Entity/GenericMath/IHasAbsoluteValue.cs
@@ -5,12 +5,6 @@
 // Website: https://am.angouri.org.
 //
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AngouriMath.Core;
 
 
